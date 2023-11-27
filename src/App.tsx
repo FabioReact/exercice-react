7e952e5bdb7543c9c0e739bc446a9cf035c0e11a
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2023-11-27 13:36:36 +0100

    feat: correction apres exercice 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import UserCard from './UserCard'
-import user from './data/user'
-import users from './data/users'
+import UserCard from "./UserCard";
+import user from "./data/user";
+import users from "./data/users";
+import { User } from "./types/User";
 
-const App = (): JSX.Element => {
+const App = (): React.ReactElement => {
   return (
     <>
       <h1>Exercices React</h1>
       <h2>Exercice 1: Passer des props à Usercard</h2>
-      <UserCard />
-      <h2>Exercice 2: Faite une boucle afin d'afficher une UseCard par utilisateur</h2>
+      <UserCard name={user.name} house={user.house} img={user.img} />
+      <h2>
+        Exercice 2: Faite une boucle afin d'afficher une UseCard par utilisateur
+      </h2>
+      {users.map(({ name, house, img, id }: User) => (
+        <UserCard key={id} name={name} house={house} img={img} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -1,24 +1,29 @@
-type AvatarProps = {
+import { User } from "./types/User";
 
+type AvatarProps = {
+	image: string;
+	alternative: string;
 }
 
 const Avatar = (props: AvatarProps): JSX.Element => {
 	return (
-		<img src="" alt="Profil of" />
+		<img src={props.image} alt={props.alternative} />
 	)
 }
 
-type UserCardProps = {
+type UserCardProps = Omit<User, 'id'>
 
-}
-
-const UserCard = (props: UserCardProps): JSX.Element => {
+const UserCard = ({ house, img, name }: UserCardProps): JSX.Element => {
+	// const { house, img, name } = props
+	// const house = props.house
+	// const img = props.img
+	// const name = props.name
 	return (
 		<div className="card">
-			<Avatar />
+			<Avatar image={img} alternative={`Profil of ${name} ${house}`} />
 			<div>
-				<p>Prénom: </p>
-				<p>House: </p>
+				<p>Prénom: {name}</p>
+				<p>House: {house}</p>
 			</div>
 		</div>
 	)
